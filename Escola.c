4eba8e3c8aa7627e1217e5199a531b626330f678
Samuel Lúcio <102866875+samucalucio1997@users.noreply.github.com>
2023-04-03 11:30:55 -0300

    Add files via upload

--- Escola.c
+++ Escola.c
@@ -13,21 +13,29 @@ void inserir(int peso, struct Aluno *Aluno,int nota,int con){
   con++; 
 }
 void print_array_struct(struct Aluno *aluno, int qtd){
-     printf("%d\n",aluno->matricula);
+     printf("%d\n",&aluno->matricula);
      for(int c=0;c<qtd;c++){
         printf("%d ",(aluno->notas[c])*(aluno->pesos[c]));
      }
 }
+int media_alun(struct Aluno *aluno, int qtd){
+    
+}
 int main(){
   int qtd,nota,peso;
-  struct Aluno *aluno;
-  scanf("%d",&qtd);
-  aluno->notas = (int *) malloc(sizeof(int)*qtd);
-  aluno->pesos = (int *) malloc(sizeof(int)*qtd);
-  scanf("%d",&aluno->matricula);
-  for(int i=0;i<qtd;i++){
-    scanf("%d %d",&nota,&peso);
-    inserir(peso,aluno,nota,i);
+  struct Aluno *aluno,*gamb;
+  int al;
+  scanf("%d",&al);
+  aluno  = (struct Aluno *) malloc(sizeof(struct Aluno)*al);
+  for(int c=0;c<al;c++){
+    scanf("%d",&qtd);
+    for(int i=0;i<qtd;i++){
+      aluno->notas = (int *) malloc(sizeof(int)*qtd);
+      aluno->pesos = (int *) malloc(sizeof(int)*qtd);
+      // scanf("%lld",aluno->matricula);
+      scanf("%d %d",&nota,&peso);
+      inserir(peso,aluno+c,nota,i);
+    }
   }
   print_array_struct(aluno,qtd);
 }
