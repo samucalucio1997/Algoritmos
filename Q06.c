8c26e2457811fe13187e7992a5a28e2f5df4028b
Samuel Lúcio <102866875+samucalucio1997@users.noreply.github.com>
2023-04-24 15:15:20 -0300

    questão 6 C

--- Q06.c
+++ Q06.c
@@ -1,18 +1,75 @@
 #include <stdio.h>
-int con(int g, int mul,int con){
-    if(g%mul==0){
-       con++;
-    }
-    return con;
+#include <stdlib.h>
+struct array_list{
+int *data;
+unsigned int size;
+unsigned int capacity;
+};
+void array_list_increase_capacity(struct array_list *list){
+if(list->capacity==list->size){
+      list->capacity=list->capacity+10;
+      struct array_list *new_list = (struct array_list *) malloc(sizeof(struct array_list)*list->capacity);
+      struct array_list *antigo; 
+      antigo =list;
+      for(int i=0;i<list->size;++i){
+           *(new_list->data + i)=*(list->data + i);
+      }
+      list=new_list;
+      free(antigo);
+}
+fprintf(stderr,"Error on memory allocation.\n");
+exit(-1);
+}
+struct array_list * array_list_create(){
+struct array_list *new_list;
+new_list = (struct array_list*)malloc(sizeof(struct array_list));
+if (new_list==0){ /* Error */
+fprintf(stderr,"Error on memory allocation.\n");
+exit(-1);
+}
+new_list->capacity = 10;
+new_list->size = 0;
+new_list->data = (int*)malloc(sizeof(int)*new_list->capacity);
+if (new_list->data==0){ /* Error */
+fprintf(stderr,"Error on memory allocation.\n");
+exit(-1);
+}
+return new_list;
+}
+void array_list_read_until(struct array_list *list, int end_reading){
+int x;
+while (scanf("%d",&x),x!=end_reading){
+if (list->size==list->capacity)
+array_list_increase_capacity(list);
+list->data[list->size++] = x;
+}
+}
+void array_list_print(struct array_list *list){
+printf("[");
+if (list->size>0){
+printf("%d",list->data[0]);
+for (int i=1 ; i<list->size ; ++i)
+printf(", %d",list->data[i]);
+}
+printf("]");
+}
+void array_list_append(struct array_list *list,int p){
+      int l = list->size;
+      if(list->capacity==list->size){
+            array_list_increase_capacity(list);
+      }
+      int i=0;
+      list->data[list->size++]=p;
 }
 int main(){
-    int qtd,mul,cont=0;
-    scanf("%d %d", &mul, &qtd);
-    int p[qtd];
-    for(int c=0;c<qtd;c++){
-        scanf("%d ",&p[c]);
-        cont = con(p[c],mul,cont);
-    }
-    printf("%d",cont);
-    return 0;
+struct array_list *list01 = array_list_create();
+array_list_read_until(list01,-1);
+int k;
+scanf("%d",&k);
+array_list_append(list01,k);
+array_list_print(list01);
+printf("\n");
+free(list01->data);
+free(list01);
+return 0;
 }
